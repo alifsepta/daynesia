b9d1e7d44464e1a98bd4b746baea14edc0953de7
alifsepta <alif.techdev@gmail.com>
2022-06-29 15:34:52 +0700

    add new file

---
+++ index.js
@@ -0,0 +1,16 @@
+// Get all the images
+image_array = [
+    'kambing.png',
+    'sapi.png',
+  ]
+  
+  function get_random_image(){
+    // Get a random index
+    random_index = Math.floor(Math.random() * image_array.length);
+  
+    // Get an image at the random_index
+    selected_image = image_array[random_index]
+  
+    // Display the image
+    document.getElementById('image_shower').src = `./images/${selected_image}`
+  }
